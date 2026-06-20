944f2af7c88ddc98d70f2751890f8c3c17e3d11e
LE MINH CUONG <lmcuong999@gmail.com>
2026-06-20 12:59:41 +0700

    rewrite __init__.py pipeline

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,6 +1,7 @@
-from checkpointing import save_checkpoint, load_checkpoint
-from dynamic_mapper import map_answer, match_number, clean_text
-from io_handler import load_test_data, save_predictions
+from .checkpointing import load_checkpoint, save_checkpoint
+from .dynamic_mapper import clean_text, map_answer, match_number
+from .io_handler import load_test_data, save_predictions
+from .majority_voting import run_majority_voting
 
 __all__ = [
     "save_checkpoint",
@@ -10,4 +11,5 @@ __all__ = [
     "clean_text",
     "load_test_data",
     "save_predictions",
+    "run_majority_voting",
 ]
